50f2d2d1f9eacafe6a6306e1499ef76a4c71afa7
Karsten Schöke <lgbff@users.noreply.github.com>
2017-02-22 10:23:06 +0100

    insert wato rule for agent bakery

---
+++ web/plugins/wato/agent_bakery_yum.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- encoding: utf-8; py-indent-offset: 4 -*-
+
+group = "agents/" + _("Agent Plugins")
+
+register_rule(group,
+    "agent_config:yum",
+    Alternative(
+        title = _("YUM (Community Version) updates (Linux)"),
+        help = _("This will deploy the agent plugin <tt>yum</tt>. This will activate the "
+                 "check <tt>yum</tt> on RedHat based hosts and monitor pending normal and security updates."),
+        style = "dropdown",
+        elements = [
+            Dictionary(
+                title = _("Deploy the YUM plugin"),
+                elements = [
+                    ( "interval",
+                      Age(title = "Interval for checking updates"),
+                    ),
+                ],
+                optional_keys = False,
+            ),
+            FixedValue(None, title = _("Do not deploy the YUM plugin"), totext = _("(disabled)")),
+        ],
+        default_value = { "interval": 129600, },
+    )
+)
