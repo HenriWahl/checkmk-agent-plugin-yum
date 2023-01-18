198cc35460f4d041ebea2059b52a3494d2ebcd61
Robert Sander <r.sander@heinlein-support.de>
2023-01-18 10:20:36 +0100

    adds handling for raw edition
    fixes issue described in https://forum.checkmk.com/t/error-message-upgrading-2-1-0p9cre-to-2-1-0p19cre-cmk-gui-cee/36367

--- web/plugins/wato/yum_cee.py
+++ web/plugins/wato/yum_cee.py
@@ -1,42 +1,47 @@
 #!/usr/bin/env python3
 # -*- encoding: utf-8; py-indent-offset: 4 -*-
 
-from cmk.gui.i18n import _
-from cmk.gui.plugins.wato import (
-    HostRulespec,
-    rulespec_registry,
-)
-from cmk.gui.cee.plugins.wato.agent_bakery.rulespecs.utils import RulespecGroupMonitoringAgentsAgentPlugins
-from cmk.gui.valuespec import (
-    Alternative,
-    Age,
-    Dictionary,
-    FixedValue,
-)
-
-def _valuespec_agent_config_yum():
-    return Alternative(
-        title = _("Yum (Community Version) updates (Linux)"),
-        help = _("This will deploy the agent plugin <tt>yum</tt>. This will activate the "
-                 "check <tt>zypper</tt> on redHat based hosts and monitor pending normal and security updates."),
-        elements = [
-            Dictionary(
-                title = _("Deploy the Yum plugin"),
-                elements = [
-                    ( "interval",
-                      Age(title = "Interval for checking for updates"),
-                    ),
-                ],
-                optional_keys = False,
-            ),
-            FixedValue(None, title = _("Do not deploy the Yum plugin"), totext = _("(disabled)")),
-        ],
-        default_value = { "interval": 129600, },
+try:
+    from cmk.gui.i18n import _
+    from cmk.gui.plugins.wato import (
+        HostRulespec,
+        rulespec_registry,
+    )
+    from cmk.gui.cee.plugins.wato.agent_bakery.rulespecs.utils import RulespecGroupMonitoringAgentsAgentPlugins
+    from cmk.gui.valuespec import (
+        Alternative,
+        Age,
+        Dictionary,
+        FixedValue,
     )
 
-rulespec_registry.register(
-    HostRulespec(
-        group=RulespecGroupMonitoringAgentsAgentPlugins,
-        name="agent_config:yum",
-        valuespec=_valuespec_agent_config_yum,
-    ))
+    def _valuespec_agent_config_yum():
+        return Alternative(
+            title = _("Yum (Community Version) updates (Linux)"),
+            help = _("This will deploy the agent plugin <tt>yum</tt>. This will activate the "
+                     "check <tt>zypper</tt> on redHat based hosts and monitor pending normal and security updates."),
+            elements = [
+                Dictionary(
+                    title = _("Deploy the Yum plugin"),
+                    elements = [
+                        ( "interval",
+                          Age(title = "Interval for checking for updates"),
+                        ),
+                    ],
+                    optional_keys = False,
+                ),
+                FixedValue(None, title = _("Do not deploy the Yum plugin"), totext = _("(disabled)")),
+            ],
+            default_value = { "interval": 129600, },
+        )
+
+    rulespec_registry.register(
+        HostRulespec(
+            group=RulespecGroupMonitoringAgentsAgentPlugins,
+            name="agent_config:yum",
+            valuespec=_valuespec_agent_config_yum,
+        ))
+
+except ModuleNotFoundError:
+    # RAW edition
+    pass
