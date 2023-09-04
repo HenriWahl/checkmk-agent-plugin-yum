63cdc108e9e0eb7694c93da02fa5bb470e72ef5c
Timo Klecker <klecker@decoit.de>
2023-09-04 17:16:01 +0200

    Rewrite to new CheckMK 2.0 Check API
    This commit introduces the new check API to the yum check.

--- lib/python3/cmk/base/cee/plugins/bakery/yum.py
+++ lib/python3/cmk/base/cee/plugins/bakery/yum.py
@@ -6,10 +6,12 @@ from typing import Any, Dict
 
 from .bakery_api.v1 import FileGenerator, OS, Plugin, register
 
+
 def get_yum_files(conf: Dict[str, Any]) -> FileGenerator:
     yield Plugin(base_os=OS.LINUX,
                  source=Path("yum"),
                  interval=conf.get("interval"))
+
 
 register.bakery_plugin(
     name="yum",
