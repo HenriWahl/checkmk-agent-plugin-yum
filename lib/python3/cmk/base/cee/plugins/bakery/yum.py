4f1d0cacdd6dc93c253a9b88b86ab65540151def
SwampdogMash <35781133+swampdogmash@users.noreply.github.com>
2025-05-12 11:21:25 +0100

    Update yum.py for new api methods
    Updated the file deployer to use the latest version of the api structure.

--- lib/python3/cmk/base/cee/plugins/bakery/yum.py
+++ lib/python3/cmk/base/cee/plugins/bakery/yum.py
@@ -2,18 +2,25 @@
 # -*- encoding: utf-8; py-indent-offset: 4 -*-
 
 from pathlib import Path
-from typing import Any, Dict
+from typing import Any
 
-from .bakery_api.v1 import FileGenerator, OS, Plugin, register
+from cmk.base.cee.plugins.bakery.bakery_api.v1 import FileGenerator, OS, Plugin, register
 
 
-def get_yum_files(conf: Dict[str, Any]) -> FileGenerator:
-    yield Plugin(base_os=OS.LINUX,
-                 source=Path("yum"),
-                 interval=conf.get("interval"))
+def get_yum_files(conf: Any) -> FileGenerator:
+#    if conf.get('deploy', 'nointerval') == 'nointerval':
+#      return
+  if conf.get('interval') is not None:
+    interval=conf.get('interval')
+  elif conf.get('deploy', 'interval')[1] is not None:
+    interval=conf.get('deploy', 'interval')[1]
 
+  yield Plugin(base_os=OS.LINUX,
+               source=Path('yum'),
+               interval=int(interval)
+  )
 
 register.bakery_plugin(
-    name="yum",
+    name='yum',
     files_function=get_yum_files,
 )
